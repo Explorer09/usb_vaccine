Explorer09/usb_vaccine

commit 60656ad717b5e285da10f2a78227a0ae25e3e3fe

CodingStyle: Uppercase variable letter when we use %~aI expansion.

usb_vaccine.cmd
@@ -850,10 +850,10 @@ REM Even with file system redirection in WoW64, the "IF EXIST" construct and
 REM 'attrib' command do not redirect and can be used to check the existence of
 REM real file names on disk. (Better not run cmd.exe inside if we're unsure
 REM that "Sysnative" is a redirected pseudo-directory.)
-FOR %%i IN ("%WinDir%\Sysnative") DO (
+FOR %%I IN ("%WinDir%\Sysnative") DO (
     REM "%~a1" redirects.
-    CALL :has_ci_substr "%%~ai" "d" && (
-        CALL :has_ci_substr "%%~ai" "h" || (
+    CALL :has_ci_substr "%%~aI" "d" && (
+        CALL :has_ci_substr "%%~aI" "h" || (
             IF NOT EXIST %%i (
                 %%i\cmd /d /c "%0 --no-restart !args!" && EXIT /B 0
                 SET status=!ERRORLEVEL!

usb_vaccine_zh-TW.cmd
@@ -817,10 +817,10 @@ REM Even with file system redirection in WoW64, the "IF EXIST" construct and
 REM 'attrib' command do not redirect and can be used to check the existence of
 REM real file names on disk. (Better not run cmd.exe inside if we're unsure
 REM that "Sysnative" is a redirected pseudo-directory.)
-FOR %%i IN ("%WinDir%\Sysnative") DO (
+FOR %%I IN ("%WinDir%\Sysnative") DO (
     REM "%~a1" redirects.
-    CALL :has_ci_substr "%%~ai" "d" && (
-        CALL :has_ci_substr "%%~ai" "h" || (
+    CALL :has_ci_substr "%%~aI" "d" && (
+        CALL :has_ci_substr "%%~aI" "h" || (
             IF NOT EXIST %%i (
                 %%i\cmd /d /c "%0 --no-restart !args!" && EXIT /B 0
                 SET status=!ERRORLEVEL!

commit 26694ffc6d9e7d82c36cd1548677e3347290a7c4

Correct iterator variable in main_restart_native
(Regression caused by 60656ad717b5e285da10f2a78227a0ae25e3e3fe)

usb_vaccine.cmd
@@ -879,8 +879,8 @@ FOR %%I IN ("%WinDir%\Sysnative") DO (
     REM "%%~aI" redirects.
     CALL :has_ci_substr "%%~aI" "d" && (
         CALL :has_ci_substr "%%~aI" "h" || (
-            IF NOT EXIST %%i (
-                %%i\cmd /d /c "%0 --no-restart !args!" && GOTO :main_end
+            IF NOT EXIST %%I (
+                %%I\cmd /d /c "%0 --no-restart !args!" && GOTO :main_end
                 SET status=!ERRORLEVEL!
             )
         )

usb_vaccine_zh-TW.cmd
@@ -844,8 +844,8 @@ FOR %%I IN ("%WinDir%\Sysnative") DO (
     REM "%%~aI" redirects.
     CALL :has_ci_substr "%%~aI" "d" && (
         CALL :has_ci_substr "%%~aI" "h" || (
-            IF NOT EXIST %%i (
-                %%i\cmd /d /c "%0 --no-restart !args!" && GOTO :main_end
+            IF NOT EXIST %%I (
+                %%I\cmd /d /c "%0 --no-restart !args!" && GOTO :main_end
                 SET status=!ERRORLEVEL!
             )
         )